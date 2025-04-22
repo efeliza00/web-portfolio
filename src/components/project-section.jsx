20795291cb11feebb6d0563ab4a61a4bf28e19a2
efeliza00 <evanfeliza22@gmail.com>
2025-04-22 13:05:03 +0800

    chore(project-section): add list placeholders

--- src/components/project-section.jsx
+++ src/components/project-section.jsx
@@ -1,8 +1,38 @@
 import { Element } from "react-scroll"
 import { AuroraText } from "./magicui/aurora-text"
+import { BentoCard, BentoGrid } from "./magicui/bento-grid"
 import { BlurFade } from "./magicui/blur-fade"
 
 const ProjectSection = () => {
+    const features = [
+        {
+            name: "Save your files",
+            description: "We automatically save your files as you type.",
+            href: "/",
+            cta: "Learn more",
+            background: (
+                <img className="absolute -right-20 -top-20 opacity-60" />
+            ),
+        },
+        {
+            name: "Full text search",
+            description: "Search through all your files in one place.",
+            href: "/",
+            cta: "Learn more",
+            background: (
+                <img className="absolute -right-20 -top-20 opacity-60" />
+            ),
+        },
+        {
+            name: "Multilingual",
+            description: "Supports 100+ languages and counting.",
+            href: "/",
+            cta: "Learn more",
+            background: (
+                <img className="absolute -right-20 -top-20 opacity-60" />
+            ),
+        },
+    ]
     return (
         <Element name="projects" className="bg-primary-foreground">
             <div className="container mx-auto max-w-3xl py-28 ">
@@ -11,31 +41,11 @@ const ProjectSection = () => {
                         <AuroraText>My Projects</AuroraText>
                     </h2>
                 </BlurFade>
-                <p>
-                    a passionate web developer with expertise in building
-                    modern, responsive, and user-friendly websites and
-                    applications. I specialize in [mention your key skills,
-                    e.g., front-end development (HTML, CSS, JavaScript, React),
-                    back-end development (Node.js, Python, PHP), or full-stack
-                    development]. With a strong eye for detail and a love for
-                    clean, efficient code, I create seamless digital experiences
-                    that meet both user needs and business goals. Whether it’s
-                    crafting dynamic single-page applications, optimizing
-                    performance, or solving complex technical challenges, I
-                    thrive on turning ideas into functional, visually appealing
-                    solutions.a passionate web developer with expertise in
-                    building modern, responsive, and user-friendly websites and
-                    applications. I specialize in [mention your key skills,
-                    e.g., front-end development (HTML, CSS, JavaScript, React),
-                    back-end development (Node.js, Python, PHP), or full-stack
-                    development]. With a strong eye for detail and a love for
-                    clean, efficient code, I create seamless digital experiences
-                    that meet both user needs and business goals. Whether it’s
-                    crafting dynamic single-page applications, optimizing
-                    performance, or solving complex technical challenges, I
-                    thrive on turning ideas into functional, visually appealing
-                    solutions.
-                </p>
+                <BentoGrid className="lg:grid-rows-3 mt-5">
+                    {features.map((feature) => (
+                        <BentoCard key={feature.name} {...feature} />
+                    ))}
+                </BentoGrid>
             </div>
         </Element>
     )
