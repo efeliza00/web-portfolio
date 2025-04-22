755125f558144b41ee662e252a96ce95b81b65e8
efeliza00 <evanfeliza22@gmail.com>
2025-04-22 13:03:54 +0800

    chore(about-section): update spacing

--- src/components/about-section.jsx
+++ src/components/about-section.jsx
@@ -6,7 +6,7 @@ import { TextAnimate } from "./magicui/text-animate"
 const AboutSection = ({ about }) => {
     return (
         <Element name="about" className="bg-primary-foreground">
-            <div className="container mx-auto max-w-3xl py-28">
+            <div className="container mx-auto max-w-3xl py-28 space-y-3">
                 <BlurFade delay={0.25} inView>
                     <h2 className="text-2xl font-bold tracking-tighter sm:text-4xl xl:text-5xl/none">
                         <AuroraText>About Me</AuroraText>
