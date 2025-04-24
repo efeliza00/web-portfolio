6a5fde6608b2d42170cb8e7b0c7f05b1dc8e29a8
efeliza00 <evanfeliza22@gmail.com>
2025-04-24 21:44:39 +0800

    chore: update footer desc

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,9 @@ import { AuroraText } from "./magicui/aurora-text"
 
 const Footer = ({info}) => {
   return (
-    <div className='container mx-auto py-10 bg-secondary text-center tracking-tight'>Designed by <AuroraText>{info.name}</AuroraText>.</div>
+      <div className="container mx-auto py-10 bg-secondary text-center tracking-tight">
+          Built by <AuroraText>{info.name}</AuroraText>.
+      </div>
   )
 }
 
