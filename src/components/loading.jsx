bd3b8404484a7207ebc2bed15476987c3f52427d
efeliza00 <evanfeliza22@gmail.com>
2025-04-24 14:47:54 +0800

    chore(loading): add text

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -3,8 +3,11 @@ import "ldrs/react/Ring.css"
 
 const Loading = () => {
     return (
-        <div className="flex items-center justify-center h-screen">
+        <div className="flex flex-col items-center justify-center h-screen">
             <Ring size="40" stroke="5" bgOpacity="0" speed="2" color="black" />
+            <h3 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                Wait...
+            </h3>
         </div>
     )
 }
